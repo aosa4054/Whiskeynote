8ebf7c52c138f7e8d600a27365c07be129aff781
aosa <shoara0509@gmail.com>
2018-10-21 23:05:14 +0900

    uriモチモチ

--- app/src/main/java/io/github/aosa4054/whiskeynote/editWhiskey/EditWhiskeyActivity.kt
+++ app/src/main/java/io/github/aosa4054/whiskeynote/editWhiskey/EditWhiskeyActivity.kt
@@ -2,15 +2,24 @@ package io.github.aosa4054.whiskeynote.editWhiskey
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.provider.MediaStore
+import android.util.Log
+import android.view.KeyEvent
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import androidx.core.net.toFile
+import androidx.core.net.toUri
 import io.github.aosa4054.whiskeynote.R
 import kotlinx.android.synthetic.main.activity_edit_whiskey.*
 import kotlinx.android.synthetic.main.fragment_edit_whiskey.*
@@ -21,19 +30,19 @@ import java.util.*
 
 class EditWhiskeyActivity : AppCompatActivity() {
 
+    lateinit var uri: Uri
+    private val REQUEST_CHOOSER = 100
+    private val RESULT_CROP = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_whiskey)
         setSupportActionBar(toolbar_edit_whiskey)
         toolbar_edit_whiskey.setNavigationOnClickListener { v ->
-            //確認ダイアログ
-            finish()
+            showCancelDialog()
         }
+        hogehoge.setOnClickListener { v -> getImage() }
     }
-
-    lateinit var uri: Uri
-    private val REQUEST_CHOOSER = 1000
-    private val RESULT_CROP = 782
 
     @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     fun getImage(){
@@ -55,7 +64,8 @@ class EditWhiskeyActivity : AppCompatActivity() {
 
     fun crop(photoUri: Uri){
         val intent = Intent("com.android.camera.action.CROP")
-        intent.data = photoUri
+        intent.setDataAndType(photoUri, "image/*")
+        intent.putExtra("crop", "true")
         intent.putExtra("outputX", 400)
         intent.putExtra("outputY", 400)
         intent.putExtra("aspectX", 1)
@@ -67,15 +77,24 @@ class EditWhiskeyActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        fun getRealPath(uri: Uri): Uri{
+            return if (uri.toString().startsWith("content://com.android.providers.media.documents")) {
+                Uri.parse("content://media/external/images/media/${uri.toString().split("/")[4].takeLast(6)}")
+            }else{
+                uri
+            }
+        }
+
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CHOOSER) {
             if (resultCode != Activity.RESULT_OK) return
             val resultUri = (if (data != null) data.data else uri) ?: return
-            crop(resultUri)
+            crop(getRealPath(resultUri))
         }
 
         if (requestCode == RESULT_CROP){
             if (resultCode != Activity.RESULT_OK) {
+                Toast.makeText(this, "おいおい", Toast.LENGTH_SHORT)
                 return
             }
             val mUri = (if (data != null) data.data else uri) ?: return
@@ -84,15 +103,34 @@ class EditWhiskeyActivity : AppCompatActivity() {
                     arrayOf("image/jpeg"),
                     null)
 
-            lateinit var bitmap: Bitmap
             try {
                 val sourceBitmap = MediaStore.Images.Media.getBitmap(contentResolver, mUri)
-                bitmap = Bitmap.createBitmap(sourceBitmap, 0, 0, 400, 400, null, true)
+                val bitmap = Bitmap.createBitmap(sourceBitmap, 0, 0, 400, 400, null, true)
+                val roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(resources, bitmap)
+                roundedBitmapDrawable.cornerRadius = 200f
+                editing_image.setImageDrawable(roundedBitmapDrawable)
             }catch (e: IOException) {
                 e.printStackTrace()
                 Toast.makeText(this, "画像の変換でエラーが発生しました", Toast.LENGTH_SHORT).show()
             }
-            editing_image.setImageBitmap(bitmap)
         }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return if (keyCode == KeyEvent.KEYCODE_BACK) {
+            showCancelDialog()
+            false
+        } else {
+            super.onKeyDown(keyCode, event)
+        }
+    }
+
+    private fun showCancelDialog() {
+        AlertDialog.Builder(this)
+                .setTitle("編集内容を保存しますか？")
+                .setMessage("保存した内容は後から編集、削除することができます。")
+                .setPositiveButton("保存", null) //save()
+                .setNegativeButton("破棄") { _, _ -> finish() }
+                .show()
     }
 }
--- app/src/main/java/io/github/aosa4054/whiskeynote/editWhiskey/EditWhiskeyFragment.kt
+++ app/src/main/java/io/github/aosa4054/whiskeynote/editWhiskey/EditWhiskeyFragment.kt
@@ -1,12 +1,16 @@
 package io.github.aosa4054.whiskeynote.editWhiskey
 
+import android.graphics.Outline
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
+import androidx.core.graphics.drawable.toDrawable
 import io.github.aosa4054.whiskeynote.R
+import kotlinx.android.synthetic.main.fragment_edit_whiskey.*
 
 class EditWhiskeyFragment : Fragment() {
 
@@ -18,13 +22,26 @@ class EditWhiskeyFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_edit_whiskey, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_edit_whiskey, container, false)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        //fab_save.outlineProvider = ExtendedFabOutlineProvider()
+        //fab_save.clipToOutline = true
+
         viewModel = ViewModelProviders.of(this).get(EditWhiskeyViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
+    class ExtendedFabOutlineProvider : ViewOutlineProvider() {
+
+        override fun getOutline(view: View, outline: Outline) {
+            outline.setRoundRect(0, 0, view.width, view.height, view.height / 2f)
+            view.background = R.color.colorAccent.toDrawable()
+        }
+    }
 }
