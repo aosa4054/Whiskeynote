e67df3a6edaf7b185439f730ddf00564cb9af635
shoara0509 <shoara0509@gmail.com>
2018-11-07 01:09:32 +0900

    seekbarをカスタムしたViewの作成

---
+++ app/src/main/java/io/github/aosa4054/whiskeynote/SeekBarWithValueLabel.kt
@@ -0,0 +1,38 @@
+package io.github.aosa4054.whiskeynote
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.widget.SeekBar
+import androidx.core.content.ContextCompat
+
+
+class SeekBarWithValueLabel: SeekBar {
+    constructor(context: Context): super(context){ setUpLabel(context) }
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs){ setUpLabel(context) }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr){ setUpLabel(context) }
+
+    private val textPaint = Paint()
+
+    private fun setUpLabel(context: Context){
+        textPaint.color = ContextCompat.getColor(context, R.color.colorAccent)
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.textSize = 28f
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val x = thumb.bounds.centerX() + 12f
+        val y = 28f
+
+        canvas?.drawText(progress.toString(), x, y, textPaint)
+
+        /*
+        val d: Drawable = resources.getDrawable(R.drawable.ic_place_black_24dp)
+        d.setBounds(x.toInt(), y.toInt(), width, height)
+        d.draw(canvas)
+        */
+    }
+
+}
--- app/src/main/java/io/github/aosa4054/whiskeynote/data/WhiskeyRepository.kt
+++ app/src/main/java/io/github/aosa4054/whiskeynote/data/WhiskeyRepository.kt
@@ -1,26 +1,18 @@
 package io.github.aosa4054.whiskeynote.data
 
 import android.app.Application
-import android.os.AsyncTask
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
 
 class WhiskeyRepository(application: Application) {
-    private var mWhiskeyDao: WhiskeyDao
+    private var mWhiskeyDao: WhiskeyDao = WhiskeyDatabase.getInstance(application).whiskeyDao()
     private lateinit var mAllWhiskeys: List<Whiskey>
-
-    init {
-        val db: WhiskeyDatabase = WhiskeyDatabase.getInstance(application)
-        mWhiskeyDao = db.whiskeyDao()
-        launch { mAllWhiskeys = getAllWhiskeys() }
-    }
 
     suspend fun getAllWhiskeys(): List<Whiskey>{
         return async { mWhiskeyDao.getAllWhiskeys() }.await()
     }
 
-    //fun getAllWhiskeys(): List<Whiskey> = mAllWhiskeys
     fun getScotch(): List<Whiskey> = mAllWhiskeys.filter { it.type == "スコッチ" }
     fun getJapanese(): List<Whiskey> = mAllWhiskeys.filter { it.type == "ジャパニーズ" }
     fun getAmerican(): List<Whiskey> = mAllWhiskeys.filter { it.type == "アメリカン" }
